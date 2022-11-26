bc7ed72669d0e7b991fec3f08d878bed6b7fc45e
Abrar Khan <heyabrarkhan@gmail.com>
2022-11-26 12:13:27 +0530

    PUB for images added

--- beauty_bebo/src/Components/ContentForSingleRenderPage.jsx
+++ beauty_bebo/src/Components/ContentForSingleRenderPage.jsx
@@ -6,11 +6,11 @@ export default function ConentForSingleRenderPage ( ) {
         <>
          <SimpleGrid className="ThirtyPercentBannerSimpleGrid" columns={[1,1,2]} w={{base : '95%' , md : '95%' , lg : '85%'}}>
             <Box  w={{base : '95%', md :'90%', lg : '90%'}} m='auto'>
-              <Link to={'/fragrance'}><Image m='auto' src="https://www.beautybebo.com/media/mega-menu/ayurveda_cata.jpg"/></Link>
+              <Link to={'/fragrance'}><Image m='auto' src="https://www.beautybebo.com/pub/media/mega-menu/ayurveda_cata.jpg"/></Link>
             </Box>
 
             <Box  w={{base : '95%', md :'90%', lg : '90%'}} m='auto'>
-              <Link to='/mombabycare'><Image m='auto'  src="https://www.beautybebo.com/media/mega-menu/mom_baby_cata.jpg"/></Link>
+              <Link to='/mombabycare'><Image m='auto'  src="https://www.beautybebo.com/pub/media/mega-menu/mom_baby_cata.jpg"/></Link>
             </Box>
          </SimpleGrid>
         </>
--- beauty_bebo/src/Components/ThirtyPercentOffBanner.jsx
+++ beauty_bebo/src/Components/ThirtyPercentOffBanner.jsx
@@ -6,11 +6,11 @@ export default function ThirtyPercentOff ( ){
         <>
         <SimpleGrid className="ThirtyPercentBannerSimpleGrid" columns={[1,1,2]} w={{base : '95%' , md : '95%' , lg : '85%'}}>
           <Box  w={{base : '95%', md :'90%', lg : '90%'}} m='auto'>
-            <Link to={'/mombabycare'}><Image m='auto' src="https://www.beautybebo.com/media/ads/joy_banner.gif"/></Link>
+            <Link to={'/mombabycare'}><Image m='auto' src="https://www.beautybebo.com/pub/media/ads/joy_banner.gif"/></Link>
           </Box>
 
           <Box  w={{base : '95%', md :'90%', lg : '90%'}} m='auto'>
-            <Link to='/hair'><Image m='auto'  src="https://www.beautybebo.com/media/ads/lotus_banner.gif"/></Link>
+            <Link to='/hair'><Image m='auto'  src="https://www.beautybebo.com/pub/media/ads/lotus_banner.gif"/></Link>
           </Box>
         </SimpleGrid>
         </>
